7e692498343cf8c81f02bd018fb768eeb0b54120
Vincent Boutour <bob@vibioh.fr>
2022-10-05 22:02:11 +0200

    fix: Stay on the 7.0.36 until Scaleway update
    Relates to https://github.com/minio/minio-go/issues/1702
    
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/main.go
+++ cmd/main.go
@@ -24,7 +24,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	items, err := storage.List(context.Background(), "")
+	fmt.Println(storage.CreateDir(context.Background(), "/test"))
+	fmt.Println(storage.WriteTo(context.Background(), "/test/example.txt", strings.NewReader("Streamed content"), model.WriteOpts{}))
+	fmt.Println(storage.WriteTo(context.Background(), "/test/second.txt", strings.NewReader("Fixed size content"), model.WriteOpts{Size: 18}))
+
+	fmt.Println(storage.Rename(context.Background(), "/test/", "/renamed/"))
+
+	items, err := storage.List(context.Background(), "/renamed/")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,9 +39,12 @@ func main() {
 		fmt.Printf("%+v\n", item)
 	}
 
-	fmt.Println(storage.CreateDir(context.Background(), "/test"))
-	fmt.Println(storage.WriteTo(context.Background(), "/test/example.txt", strings.NewReader("Streamed content"), model.WriteOpts{}))
-	fmt.Println(storage.WriteTo(context.Background(), "/test/second.txt", strings.NewReader("Fixed size content"), model.WriteOpts{Size: 18}))
-	fmt.Println(storage.Rename(context.Background(), "/test/", "/renamed/"))
+	if len(items) != 2 {
+		log.Fatal("too many files in bucket")
+	}
+
 	fmt.Println(storage.Rename(context.Background(), "/renamed/example.txt", "/new/test.txt"))
+
+	fmt.Println(storage.Remove(context.Background(), "/renamed"))
+	fmt.Println(storage.Remove(context.Background(), "/new"))
 }
